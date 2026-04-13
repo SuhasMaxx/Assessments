227fde1da61a2eaaff18f81d08a1cd2051677a50
SuhasMaxx <sbs.max2c@gmail.com>
2026-04-13 21:26:22 +0200

    chore: upgrade vulnerable packages

--- movie-box/.eslintrc.js
+++ movie-box/.eslintrc.js
@@ -1,5 +1,21 @@
 module.exports = {
+  root: true,
   env: {
+    node: true,
+    browser: true,
+    es2020: true,
     jest: true
-  }
+  },
+  extends: [
+    'plugin:vue/essential',
+    'eslint:recommended'
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  rules: {}
 }
